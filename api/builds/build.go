3b624461c70845cf655c93ea4c19d118463176db
Alex Suraci <suraci.alex@gmail.com>
2014-05-04 08:57:47 -0700

    omit more empty fields

--- api/builds/build.go
+++ api/builds/build.go
@@ -3,10 +3,10 @@ package builds
 import "time"
 
 type Build struct {
-	Guid      string      `json:"guid"`
-	CreatedAt time.Time   `json:"created_at"`
+	Guid      string      `json:"guid,omitempty"`
+	CreatedAt time.Time   `json:"created_at,omitempty"`
 	Image     string      `json:"image"`
-	Path      string      `json:"path"`
+	Path      string      `json:"path,omitempty"`
 	Script    string      `json:"script"`
 	Env       [][2]string `json:"env"`
 	Status    string      `json:"status,omitempty"`
